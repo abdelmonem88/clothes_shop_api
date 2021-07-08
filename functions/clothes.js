443cc981e8dbc4862b451bf51fc5bcb52c934178
abdelmonem88 <abdelmonem1888@gmail.com>
2021-07-08 19:15:20 +0200

    modifiy

--- functions/clothes.js
+++ functions/clothes.js
@@ -7,6 +7,7 @@ const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
 
 exports.handler = async (event, context) => {
  const { id } = event.queryStringParameters;
+ 
 
  if (id) {
   try {
